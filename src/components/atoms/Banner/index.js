d4ef49e419d4a90625f8e30e1a091a36f64dc3af
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2024-01-11 10:59:14 -0300

    projeto criado

---
+++ src/components/atoms/Banner/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+const Banner =()=>{
+    return(
+        <div className="banner">
+            
+        </div>
+    )
+}
+export default Banner
---
+++ src/pages/App.js
@@ -0,0 +1,10 @@
+import '../App.css';
+import Banner from '../components/atoms/Banner';
+
+function App() {
+  return (
+    <Banner/>
+  );
+}
+
+export default App;
